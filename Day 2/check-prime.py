32b41359aaa0449bf7ab1f3a2e14bfcc128766ea
Goutam-aswani <goutamaswani43@gmail.com>
2025-06-04 02:51:17 -0700

    Create check-prime.py with pep8 style

---
+++ Day 2/check-prime.py
@@ -0,0 +1,21 @@
+input1 = int(input("enter a number: "))
+
+
+def is__prime(a):
+    if a <= 1:
+        return False
+    if a == 2:
+        return True
+    if a % 2 == 0:
+        return False
+    m = int(a**0.5)+1
+    for i in range(3, m, 2):
+        if a % i == 0:
+            return False
+    return True
+
+
+if is__prime(input1):
+    print(f"{input1} is a prime number")
+elif is__prime(input1):
+    print(f"{input1} is not a prime number")
